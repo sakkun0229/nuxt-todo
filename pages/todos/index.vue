6c347578e6f2a9b3662034b93b033bf2d77eebfb
Takahisa Sakuma <tsheadbang@gamil.com>
2020-04-08 17:56:51 +0900

    add todo dir

---
+++ pages/todos/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+    <h1>TODO APP</h1>
+    <ul>
+      <li>{{ msg }}</li>
+      <li>hoge</li>
+      <li>hoge</li>
+      <li>hoge</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello hoge!'
+    }
+  }
+}
+</script>
+
+<style></style>
