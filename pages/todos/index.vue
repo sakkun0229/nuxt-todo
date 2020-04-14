ab0952c44bfafb10be36d4241fb2fd3952b52587
Takahisa Sakuma <tsheadbang@gamil.com>
2020-04-14 19:55:25 +0900

    見た目調整

--- pages/todos/index.vue
+++ pages/todos/index.vue
@@ -4,24 +4,39 @@
       <v-col cols="12" sm="10" md="8">
         <h1 align="center" class="display-4 white--text">TODO APP</h1>
       </v-col>
+    </v-row>
+
+    <v-row justify="center">
       <v-col cols="12" sm="10" md="8">
-        <v-card class="elevation-10">
-          <v-text-field @keyup.enter="addTodo" v-model="newTodo" placeholder="add task..." color="blue" class="mx-5"></v-text-field>
+        <v-card class="elevation-10 px-1">
+          <v-row no-gutters align="center">
+            <v-col cols="10" class="pl-2 re">
+              <v-text-field @keyup.enter="addTodo" v-model="newTodo" placeholder="add task..." color="blue" class=""> </v-text-field>
+            </v-col>
+            <v-col cols="2" class="text-center yello">
+              <v-btn color="green" x-small outlined>Add</v-btn>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
 
     <v-row justify="center">
       <v-col cols="12" sm="10" md="8">
-        <v-card class="elevation-10 mb-2" v-for="todo in todos" :key="todo.id">
-          <v-card-title>
-            <v-checkbox :input-value="todo.done" @change="toggle(todo)" color="blue"></v-checkbox>
-            <span :class="{ done: todo.done }">{{ todo.text }}</span>
-            <v-spacer></v-spacer>
-            <v-card-actions>
-              <v-btn @click="deleteTodo(todo)" color="orange darken-4" text>Delete</v-btn>
-            </v-card-actions>
-          </v-card-title>
+        <v-card class="elevation-10 mb-2 px-2 pr-" v-for="todo in todos" :key="todo.id">
+          <v-row no-gutters align="center" justify="space-between">
+            <v-col cols="1">
+              <v-checkbox :input-value="todo.done" @change="toggle(todo)" color="light-blue"></v-checkbox>
+            </v-col>
+            <v-col cols="9" sm="9">
+              <v-card-title>
+                <span :class="{ done: todo.done }">{{ todo.text }}</span>
+              </v-card-title>
+            </v-col>
+            <v-col cols="2" class="text-center">
+              <v-btn @click="deleteTodo(todo)" color="orange darken-2" x-small outlined>Delete</v-btn>
+            </v-col>
+          </v-row>
         </v-card>
       </v-col>
     </v-row>
