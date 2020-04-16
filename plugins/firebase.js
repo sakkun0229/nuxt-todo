cb7b26ab5db08d55d30ad2a880b2b6efe5394d7c
Takahisa Sakuma <tsheadbang@gamil.com>
2020-04-16 19:27:05 +0900

    fix const positoin

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+}
+
 if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-  }
   firebase.initializeApp(firebaseConfig)
 }
 const db = firebase.firestore()
